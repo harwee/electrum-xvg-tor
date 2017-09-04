46dd3d1f2a0b8a99832b05359c25b4760b85d220
Sri Harsha <hars4646@gmail.com>
2017-09-04 14:07:47 +0530

    added tor dir to build and added dependency libraries for ubuntu build

--- cx_setup.py
+++ cx_setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 # create executable using command `python cx_setup.py build`
 
 
-buildOptions = dict(packages = ["idna","shutil","ltc_scrypt","argparse","dns","gui","dbhash","dumbdbm","ConfigParser"], excludes = [], include_files=["lib","electrum-xvg.conf"])
+buildOptions = dict(packages = ["idna","shutil","ltc_scrypt","argparse","dns","gui","dbhash","dumbdbm","ConfigParser","mmap","plugins"], excludes = [], include_files=["tor","lib","electrum-xvg.conf"])
  
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
